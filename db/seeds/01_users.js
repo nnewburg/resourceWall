c6c385cb5347ff81304b09738cdc80e98e2e871e
kcdporter <kiracdporter@gmail.com>
2019-03-31 17:36:43 +0000

    major styling changes

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice', email:'alice@gmail.com', password:'123', profile_image:''}),
-        knex('users').insert({id: 2, name: 'Bob', email:'bob@gmail.com', password:'123', profile_image:''}),
-        knex('users').insert({id: 3, name: 'Don', email:'don@gmail.com', password:'123', profile_image:''}),
+        knex('users').insert({id: 1, name: 'Dr. Facilier', email:'voodooman@gmail.com', password:'1', profile_image:''}),
+        knex('users').insert({id: 2, name: 'Buzz Lightyear', email:'buzzbuzz@gmail.com', password:'1', profile_image:''}),
+        knex('users').insert({id: 3, name: 'Christopher Robin', email:'goodpooh@gmail.com', password:'1', profile_image:''}),
         knex.raw(`SELECT setval('users_id_seq', 3, true)`)
       ]);
     });
