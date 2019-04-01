4b732d006a82f3db6bede3afc80e9ec52b5902e5
kcdporter <kiracdporter@gmail.com>
2019-04-01 04:57:59 +0000

    a better elephant profile added, seeds set for presentation

--- db/seeds/06_resource_keywords.js
+++ db/seeds/06_resource_keywords.js
@@ -3,10 +3,14 @@
     return knex('resource_keywords').del()
       .then(function () {
         return Promise.all([
-          knex('resource_keywords').insert({keyword_id: 1, resource_id: 1}),
-          knex('resource_keywords').insert({keyword_id: 3, resource_id: 2}),
-          knex('resource_keywords').insert({keyword_id: 2, resource_id: 3}),
-          knex('resource_keywords').insert({keyword_id: 1, resource_id: 3})
+          knex('resource_keywords').insert({keyword_id: 2, resource_id: 1}),
+          knex('resource_keywords').insert({keyword_id: 5, resource_id: 2}),
+          knex('resource_keywords').insert({keyword_id: 5, resource_id: 3}),
+          knex('resource_keywords').insert({keyword_id: 4, resource_id: 5}),
+          knex('resource_keywords').insert({keyword_id: 3, resource_id: 6}),
+          knex('resource_keywords').insert({keyword_id: 5, resource_id: 2}),
+          knex('resource_keywords').insert({keyword_id: 6, resource_id: 4}),
+          knex('resource_keywords').insert({keyword_id: 7, resource_id: 4}),
         ]);
       });
   }
--- db/seeds/07_comments.js
+++ db/seeds/07_comments.js
@@ -3,9 +3,9 @@
     return knex('comments').del()
       .then(function () {
         return Promise.all([
-          knex('comments').insert({user_id: 1, resource_id: 3, content: 'hi!'}),
-          knex('comments').insert({user_id: 1, resource_id: 2, content: 'I like facebook'}),
-          knex('comments').insert({user_id: 2, resource_id: 3, content: 'lighthouse'})
+          knex('comments').insert({user_id: 1, resource_id: 1, content: "Weird"}),
+          knex('comments').insert({user_id: 1, resource_id: 2, content: 'Thanks for the info'}),
+          knex('comments').insert({user_id: 2, resource_id: 3, content: 'SO CUTE!!!'})
         ]);
       });
   }
